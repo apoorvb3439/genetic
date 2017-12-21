f22a2ef0a35a69c27cccf263257ae02499d741aa
Apoorv <apoorvb3439@gmail.com>
2017-12-21 19:19:02 +0530

    Adding Updated Files
    Path Finding Algo

---
+++ obstacle.js
@@ -0,0 +1,47 @@
+let obstacles=[];
+let obstacleN=3;
+let obstacleW=10;
+let obstacleH=300;
+
+function hitObstacles(rocketPos){
+    for(let ob of obstacles){
+        if(hit(rocketPos,ob)){
+            return true;
+        }
+    }
+    return false;
+}
+
+function createObstacles(){
+    for(let i=0; i<obstacleN; ++i){
+        obstacles.push(createVector(giveRandom(10,width-10),giveRandom(20,height-20)));
+    }
+}
+
+function drawObstacles(){
+    for(let o of obstacles){
+        drawObstacle(o);
+    }
+}
+
+function drawObstacle(o){
+    fill(255,0,0,150);
+    push();
+    translate(o.x,o.y);
+    rectMode(CENTER);
+    rect(0,0,obstacleW,obstacleH);
+    pop();
+}
+
+function hit(rocketPos,o){
+    if(dist(o.x,0,rocketPos.x,0)<=obstacleW/2+rocketW/2){
+        if(dist(0,o.y,0,rocketPos.y)<=obstacleH/2+rocketH/2){
+            return true;
+        }
+    }
+    return false;
+}
+
+function giveRandom(start, end){
+    return start+(Math.random()*(end-start));
+}
---
+++ sketch.js
@@ -0,0 +1,212 @@
+let population;
+let number=800;
+let generations;
+let generationP;
+let lifespan=400;
+let year=0;
+let p;
+let dest;
+let src;
+let srcSize=40;
+let destSize=srcSize;
+let maxFitP;
+let mutationRate=0.01;
+let maxForce=0.6;
+let rocketH=6;
+let rocketW=20;
+
+function setup() {
+	createCanvas(windowWidth,windowHeight);
+	src=createVector(100,100);
+	dest=createVector(width-100,height-100);
+	generations=0;
+	p=createP();
+	generationP=createP("Generations : ");
+	maxFitP=createP("MaxFit : ");
+	createObstacles();
+	population=new Population();
+
+}
+
+function drawSrcDest(){
+	fill(0,255,0,150);
+	rect(dest.x,dest.y,srcSize,srcSize);
+	rect(src.x,src.y,destSize,destSize);
+}
+
+function draw() {
+	background(250);
+	drawSrcDest();
+	drawObstacles();
+	population.run();
+
+	year++;
+	if(year>lifespan){
+		generations++;
+		year=0;
+	//	population=new Population();
+
+		population.evaluate();
+		population.selection();
+
+	}
+	p.html("Year : "+year);
+	generationP.html("Generations : "+generations);
+	if(year==lifespan){
+	for(let r of population.rockets){
+		if(r.isCrashed==false){
+			console.log(r);
+		}
+	}
+	}
+}
+
+function Rocket(pos,vel,dna){
+	this.done=false;
+	this.isCrashed=false;
+	this.pos=pos||createVector();
+	this.vel=vel||createVector();
+	this.acc=createVector();
+	this.dna=dna||new Dna();
+
+	this.applyForce= function(force){
+		this.acc.add(force);
+	}
+
+	this.update=function(){
+		if(!this.done&&!this.isCrashed){
+			this.applyForce(this.dna.genes[year]);
+			this.vel.add(this.acc);
+			this.pos.add(this.vel);
+			this.acc.mult(0);
+
+			if(dist(dest.x,0,this.pos.x,0)<=destSize/2+rocketW/2){
+		        if(dist(0,dest.y,0,this.pos.y)<=destSize/2+rocketH/2){
+		            this.done=true;
+		        }
+		    }
+			if(this.pos.x>width||this.pos.x<0||this.pos.y>height||this.pos.y<0){
+				this.isCrashed=true;
+			}
+			if(hitObstacles(this.pos)){
+				this.isCrashed=true;
+			}
+		}
+	}
+
+	this.draw=function(){
+		fill(0,0,0,120);
+		push();
+		translate(this.pos.x,this.pos.y);
+		rotate(this.vel.heading());
+		rectMode(CENTER);
+		rect(0,0,rocketW,rocketH);
+		pop();
+	}
+
+	this.calcFitness=function(){
+		let d= dist(this.pos.x,this.pos.y,dest.x,dest.y);
+		this.fitness=1/d;
+		this.fitness=pow(this.fitness,4);
+		if(this.done){
+			this.fitness*=100;
+		}
+		if(this.isCrashed){
+			this.fitness/=10;
+		}
+	}
+}
+
+
+function Population(){
+	this.rockets=[];
+	this.popSize=number;
+	this.matingPool=[];
+
+
+	for(i=0;i<this.popSize;++i){
+		let startVel=createVector();//Math.random(),Math.random());
+		this.rockets[i]=new Rocket(createVector(src.x,src.y),startVel);
+	}
+	this.matingPool=[];
+	this.maxFit=0;
+	this.run=function(){
+		for(i=0;i<this.popSize;++i){
+			this.rockets[i].update();
+			this.rockets[i].draw();
+		}
+	}
+
+	this.evaluate=function(){
+		for(let i=0; i<this.popSize; ++i){
+			this.rockets[i].calcFitness();
+			if(this.rockets[i].fitness>this.maxFit){
+				this.maxFit=this.rockets[i].fitness;
+			}
+		}
+		maxFitP.html("MaxFit : "+this.maxFit);
+		for(let i=0; i<this.popSize; ++i){
+			if(Number(this.maxFit)!=0){
+			this.rockets[i].fitness/=this.maxFit;
+			}
+		}
+
+
+		for(let i=0; i<this.popSize; ++i){
+			var n=this.rockets[i].fitness*100;
+			for(let j=0; j<n ; j++){
+				this.matingPool.push(this.rockets[i]);
+			}
+		}
+	}
+
+	this.selection=function(){
+		let newPopulation=[];
+		for(let i=0; i<this.rockets.length; ++i){
+			let parentA=random(this.matingPool).dna;
+			let parentB=random(this.matingPool).dna;
+			let childDna=parentA.crossOver(parentB);
+			childDna.mutation();
+			newPopulation[i]=new Rocket(createVector(src.x,src.y),createVector(),childDna);
+		}
+		this.rockets=newPopulation;
+	}
+
+}
+
+function Dna(genes){
+	if(genes==undefined)
+	{
+		this.genes=[];
+		for(let i=0; i<lifespan; ++i){
+			let force=p5.Vector.random2D();
+			force.setMag(maxForce);
+			this.genes.push(force);
+		}
+	}else{
+		this.genes=genes;
+	}
+	this.crossOver=function(partner){
+		let newgenes=[];
+		let mid=floor(random(this.genes.length));
+
+		for(let i=0; i<this.genes.length; i++){
+			if(i<=mid){
+				newgenes[i]=this.genes[i];
+			}else{
+				newgenes[i]=partner.genes[i];
+			}
+		}
+
+		return new Dna(newgenes);
+	}
+	this.mutation=function() {
+		for(let i=0; i<this.genes.length; ++i){
+			if(random(1)<mutationRate){
+				let v=p5.Vector.random2D();
+				v.setMag(maxForce);
+				this.genes[i]=v;
+			}
+		}
+	}
+}
